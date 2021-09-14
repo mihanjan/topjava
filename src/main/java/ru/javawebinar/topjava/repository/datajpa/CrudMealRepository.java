6edc681fcb232577b62169ae5803fc70f4708c0c
mj <itfdotroyl@yandex.ru>
2021-09-14 10:32:13 +0300

    6 01 HW5 data jpa

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -5,3 +5,7 @@ import ru.javawebinar.topjava.model.Meal;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 }
+
+    @Query("SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDate AND m.dateTime < :endDate ORDER BY m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
+}
